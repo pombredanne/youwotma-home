4e691b5112b635e931ee330df59cdfeac9f6649c
David Bengoa <david@bengoarocandio.com>
2011-03-08 16:32:15 +0100

    imgur.com / configuracion kernel

--- dev/ratpoison/screenshoot.sh
+++ dev/ratpoison/screenshoot.sh
@@ -3,14 +3,24 @@ scrot -q 100 -d 01 "$fn"
 ratpoison -c "echo Screenshot saved!"
 /home/carl/dev/ratpoison/crop.py "$fn"
 
-accion=`zenity --list --column 'accion' 'twitpic' 'portapapeles' 'inspiracion' 'ver'`
+accion=`zenity --list --column 'accion' 'imgur' 'twitpic' 'portapapeles' 'inspiracion' 'ver'`
+
+
 
 if [ "$accion" = "twitpic" ]
 then
-  /home/carl/dev/ratpoison/twitpic.py "$fn"
+    /home/carl/dev/ratpoison/twitpic.py "$fn"
+elif [ "$accion" = "imgur" ]
+then
+    curl -F "image=@$fn" -F "key=ed6bd99a77b8b791f647f6a2783ab051" http://api.imgur.com/2/upload.json > "$fn".upload
+    url=`node /home/carl/dev/ratpoison/imgurparse.js "$fn".upload`
+    if zenity --question --text $url
+    then
+        echo $url | xsel -i -b
+    fi
 elif [ "$accion" = "portapapeles" ]
 then
-  echo $fn | xsel -i -b
+    echo $fn | xsel -i -b
 elif [ "$accion" = "inspiracion" ]
 then
     name=`zenity --entry`
