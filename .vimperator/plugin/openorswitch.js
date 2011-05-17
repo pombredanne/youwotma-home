d68a865a7fcfbee295d8970e8b09b5ccaa90aab3
David Bengoa <david@bengoarocandio.com>
2011-05-17 16:11:35 +0200

    vimperator script modification

--- .vimperator/plugin/openorswitch.js
+++ .vimperator/plugin/openorswitch.js
@@ -20,21 +20,32 @@ var PLUGIN_INFO =
   ]]></detail>
 </VimperatorPlugin>;
 // }}}
+
+function getTabArg(tab,arg){
+    try{
+        switch(arg){
+            case "domain": return tab.uri.host;
+            case "url": return tab.uri.spec;
+        }
+    }catch(e){}
+    return "";
+}
+
 commands.addUserCommand ("switchoropen", "Switch or open",
     function (args) {
-        if(args.length != 2){
+        if(args.length != 3){
             liberator.echo("Numero incorrecto de argumentos");
         }else{
             var tabs = Application.activeWindow.tabs;
-            var rx = new RegExp(args[0],"i");
+            var rx = new RegExp(args[1],"i");
             for(var i=0,l=tabs.length; i<l; ++i){
-                if(rx.test(tabs[i].uri.host)){
+                if(rx.test(getTabArg(tabs[i],args[0]))){
                     tabs[i].focus();
                     return;
                 }
             }
             var ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
-            var uri = ios.newURI(args[1], null, null);
+            var uri = ios.newURI(args[2], null, null);
             Application.activeWindow.open(uri).focus();
         }
     }
