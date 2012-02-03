fb0e2ff4e02d46c71d9c0cf8f2445c6cc7569a6e
David Bengoa <david@bengoarocandio.com>
2012-02-03 17:45:50 +0100

    register new scripts

--- shows/install.sh
+++ shows/install.sh
@@ -4,4 +4,6 @@ lnbin copy_shows_desktop.sh copy_shows_desktop
 lnbin add_magnet.sh add_magnet
 lnbin download_subs download_subs
 lnbin download_subs_all download_subs_all
+lnbin play_show.sh play_show
+lnbin show_status.sh show_status
 
