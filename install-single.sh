8a783319f675db9f49676a86f2dd816a386d8f04
David Bengoa <david@bengoarocandio.com>
2012-02-03 17:32:42 +0100

    install single script

---
+++ install-single.sh
@@ -0,0 +1,18 @@
+
+[ ! -x "$1/install.sh" ] && {
+    echo "$1/install.sh not found/ insufficient permissions"
+    exit 1
+}
+
+source utils.sh
+
+cd $1
+if [ x"$2" = 'x-y' ]
+then
+    confirm(){
+        return 0
+    }
+fi
+
+source "install.sh"
+
