5677b4c4f228e7ec4b7d176cfe832851a2ff19b9
David Bengoa <david@bengoarocandio.com>
2012-02-03 17:34:19 +0100

    pyload install script

---
+++ pyload/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Deshabilitado
+
+#confirm "Install pyload" && {
+#    cd ~/tmp
+#    wget http://get.pyload.org/get/ubuntu -O pyload.deb
+#    sudo dpkg -i pyload.deb
+#    rm pyload.deb
+#}
