3dea5d77556da1c7255793fdbb5f238437ef29e6
David Bengoa <david@bengoarocandio.com>
2012-02-13 13:23:17 +0100

    new browser-restart

--- dev/ratpoison/browser-restart.py
+++ dev/ratpoison/browser-restart.py
@@ -9,6 +9,6 @@ for id in filter(lambda x: x>=0, [
         rat.findwindow("chromium-browser"), # chrome
     ]):
     rat.chwindow(id)
-    rat.rat_cmd("meta F5")
+    rat.rat_cmd("meta C-F5")
     rat.chwindow(old) # If you don't do this inside the loop, you can end in a different frame
 
