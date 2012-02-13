099668215d7bc4a3b9c3f261b5d0290fd3c1e103
David Bengoa <david@bengoarocandio.com>
2012-02-13 13:20:14 +0100

    after save script

---
+++ dotfiles/.vim/aftersave.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ $1 == *.scss ]]
+then
+    /home/carl/dev/scripts/compilescss.py "$1"
+elif [[ $1 == *.less ]]
+then
+    /home/carl/dev/scripts/compileless.py "$1"
+fi
+
+if [[ $1 == /home/carl/dev/bacardi/* ]]
+then
+    restart-browsers
+elif [[ $1 == /home/carl/dev/instabed/* ]]
+then
+    echo yeep >> ~/tmp/tmp
+    restart-browsers
+elif [[ $1 == /home/carl/dev/4sq/* ]]
+then
+    scp $1 hoyga:/var/www/4sq &
+elif [[ $1 == /home/carl/dev/Iris/irisapp/* ]]
+then
+    cd /home/carl/dev/Iris/irisapp/
+    ./run.sh &
+fi
+
